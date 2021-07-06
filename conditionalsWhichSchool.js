5b1b948ebead3199a5e403a5c521a9c2ee572b86
HopeHappy <hope_happy@outlook.com>
2021-07-05 22:37:57 -0400

    conditionalsWhichSchool.js assignment

---
+++ conditionalsWhichSchool.js
@@ -0,0 +1,11 @@
+const whichSchool = function(age) {
+  if (age < 13) {
+      return 'Elementary School';
+  } else if (13 <= age && age <= 18) {
+      return 'Secondary School';
+  } else {
+      return 'Lighthouse Labs';
+  }
+}
+
+console.log(whichSchool(15));
