a2675dce7cf7c557310ca7424f2114eaf7d7c2a6
HopeHappy <hope_happy@outlook.com>
2021-07-06 00:45:21 -0400

    hello.js exercise

---
+++ hello.js
@@ -0,0 +1,13 @@
+const sayHello  = function () {
+    console.log("Hello, world");
+}
+  
+sayHello();
+
+const sayHi  = function (name) {
+    console.log("Hi, " + name);
+}
+
+sayHi("Caliban");
+sayHi("Miranda");
+sayHi("Ferdinand");
